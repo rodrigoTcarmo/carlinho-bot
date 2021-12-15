9b45ab11df5624b9d47c7275c1c9a74a28343981
Rodrigo Carmo <rodrigoblock@outlook.com>
2021-12-15 09:42:14 -0300

    SS-16 | Implementação de reply e rpley in thread
    Implementado as respostas do bot no canal e também as respostas do mesmo na thread da mensagem do usuário.

--- app.py
+++ app.py
@@ -23,21 +23,36 @@ class Listen:
         user_id = raw_data['user']
         channel = raw_data['channel']
         text = raw_data['text']
+        ts = raw_data['ts']
 
         data = {
             "user_id": user_id,
             "channel": channel,
-            "text": text
+            "text": text,
+            "ts": ts
         }
-        A = Analytics(use_data=data)
+        A = Analytics()
+        A.get_data(use_data=data)
+
         A.analyze_message(use_split_message=A.sanitize())
+        A.delivery_thread()
 
 
 class Analytics:
-    def __init__(self, use_data):
+    def __init__(self):
+        self.split_message = None
+        self.user_id = None
+        self.channel = None
+        self.text = None
+        self.match = None
+        self.ts = None
+
+    def get_data(self, use_data):
         self.split_message = use_data['text'].split()
         self.user_id = use_data['user_id']
+        self.channel = use_data['channel']
         self.text = use_data['text']
+        self.ts = use_data['ts']
 
     def sanitize(self):
         return [w.upper() for w in self.split_message]
@@ -48,24 +63,24 @@ class Analytics:
         match_word = (set(L.dictionary()) & set(use_split_message))
 
         if match_word:
-            match = [s for s in match_word if s][0]
-            print('Achei palavra:', match)
-            Lib(use_match=match).search_books()
+            self.match = [s for s in match_word if s][0]
+            print('Achei palavra:', self.match)
+
+            from library import Library
+            print(self.match)
+
+            read_book = Library().bookshelf()
+            print(read_book[self.match])
 
         else:
             print("Não achei nada")
 
-
-class Lib:
-    def __init__(self, use_match):
-        self.match = use_match
-
-    def search_books(self):
-        from library import Library
-        print(self.match)
-
-        read_book = Library().bookshelf()
-        print(read_book[self.match])
+    def delivery_thread(self):
+        app.client.chat_postMessage(
+            channel=self.channel,
+            thread_ts=self.ts,
+            text=self.text
+        )
 
 
 if __name__ == "__main__":
